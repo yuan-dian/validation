c0d3f20e1feb6f25c4bb7ad9ade3b98ebe692aa6
原点 <467490186@qq.com>
2024-08-23 13:45:31 +0800

    feat：优化自动赋值，支持联合类型

--- src/BaseEntity.php
+++ src/BaseEntity.php
@@ -14,6 +14,8 @@ declare (strict_types=1);
 namespace yuandian;
 
 use ReflectionClass;
+use ReflectionUnionType;
+use yuandian\exception\ValidateException;
 
 abstract class BaseEntity
 {
@@ -47,28 +49,74 @@ abstract class BaseEntity
         foreach ($array as $key => $value) {
             if ($reflectionClass->hasProperty($key)) {
                 $property = $reflectionClass->getProperty($key);
-
-                // 获取属性类型
                 $propertyType = $property->getType();
-                if ($propertyType !== null) {
-                    $propertyClassName = $propertyType->getName();
-
-                    if ($propertyType->isBuiltin()) {
-                        // 对于内建类型，进行类型转换
-                        settype($value, $propertyClassName);
-                        $object->$key = $value;
-                    } elseif (class_exists($propertyClassName)) {
-                        // 对于类类型，递归转换
-                        $object->$key = $this->arrayToObject($value, $propertyClassName);
-                    }
-                } else {
-                    // 没有类型提示，直接赋值
+                // 没有指定类型，直接赋值
+                if ($propertyType === null) {
                     $object->$key = $value;
+                    continue;
+                }
+                // 检查是否为可空类型
+                $isNullable = $propertyType->allowsNull();
+
+                // 如果值为 null 且属性允许 null，则直接赋值
+                if (is_null($value) && $isNullable) {
+                    $object->$key = null;
+                    continue;
+                }
+                // 属性类型处理，支持联合类型处理
+                $types = $propertyType instanceof ReflectionUnionType ? $propertyType->getTypes() : [$propertyType];
+
+                $typeNames = [];
+                foreach ($types as $type) {
+                    $typeNames[] = $type->getName();
+                }
+
+                $valueTypeName = $this->getPhpTypeName($value);
+
+                if (in_array($valueTypeName, $typeNames)) {
+                    $object->$key = $value;
+                    continue;
+                }
+                foreach ($types as $type) {
+                    $typeName = $type->getName();
+                    if ($type->isBuiltin()) {
+                        try {
+                            settype($value, $typeName);
+                            $object->$key = $value;
+                        } catch (\Exception $exception) {
+                            throw new ValidateException("$key 类型不匹配");
+                        }
+                        break;
+                    } elseif (class_exists($typeName)) {
+                        if (!is_array($value)) {
+                            throw new ValidateException("$key 类型不匹配");
+                        }
+                        $object->$key = $this->arrayToObject($value, $typeName);
+                        break;
+                    }
                 }
             }
         }
 
         return $object;
+    }
+
+    /**
+     * @param $value
+     * @return string
+     * @date 2024/8/23 11:57
+     * @author 原点 467490186@qq.com
+     */
+    private function getPhpTypeName($value): string
+    {
+        $type = gettype($value);
+        return match ($type) {
+            'integer' => 'int',
+            'double' => 'float',
+            'boolean' => 'bool',
+            'NULL' => 'null',
+            default => $type,
+        };
     }
 
 }
