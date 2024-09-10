25d96eda5806e2470d0476c724e4ac67abf5aeb4
原点 <467490186@qq.com>
2024-09-10 17:42:49 +0800

    feat：增加Bean工具类

--- src/BaseEntity.php
+++ src/BaseEntity.php
@@ -13,11 +13,9 @@ declare (strict_types=1);
 
 namespace yuandian;
 
-use ReflectionClass;
 use ReflectionException;
-use ReflectionUnionType;
-use yuandian\attributes\Trim;
 use yuandian\exception\ParameterException;
+use yuandian\utils\BeanUtil;
 
 abstract class BaseEntity
 {
@@ -27,138 +25,10 @@ abstract class BaseEntity
      */
     public function __construct(array $data = [])
     {
-        if (empty($data)) {
-            $this->arrayToObject($data, $this);
+        if (!empty($data)) {
+            BeanUtil::arrayToObject($data, $this);
         }
     }
 
-    /***
-     * 数组转对象
-     *
-     * @param array $array
-     * @param string|object $object
-     * @return object
-     * @throws ParameterException|ReflectionException
-     * @date 2024/8/22 15:14
-     * @author 原点 467490186@qq.com
-     */
-    public function arrayToObject(array $array, string|object $object): object
-    {
-        $reflectionClass = new ReflectionClass($object);
-        // 如果$object是字符串，则创建一个新的实例
-        if (is_string($object)) {
-            $object = $reflectionClass->newInstanceWithoutConstructor();
-        }
-
-        foreach ($array as $key => $value) {
-            // 检查属性是否定义
-            if (!$reflectionClass->hasProperty($key)) {
-                continue;
-            }
-            $property = $reflectionClass->getProperty($key);
-            $propertyType = $property->getType();
-
-            // 自动去除空格的处理
-            if (is_string($value) && !empty($property->getAttributes(Trim::class))) {
-                $value = trim($value);
-            }
-
-            // 处理无类型或空值的情况
-            if ($propertyType === null || (is_null($value) && $propertyType->allowsNull())) {
-                $object->$key = $value;
-                continue;
-            }
-
-            // 属性类型处理，支持联合类型处理
-            $types = $propertyType instanceof ReflectionUnionType ? $propertyType->getTypes() : [$propertyType];
-
-            // 优先匹配值的实际类型
-            if (in_array($this->getPhpTypeName($value), array_map(fn($type) => $type->getName(), $types))) {
-                $object->$key = $value;
-                continue;
-            }
-            // 类型转换处理
-            $this->assignConvertedValue($object, $key, $value, $types);
-        }
-
-        return $object;
-    }
-
-    /**
-     * 获取PHP的原生类型名称
-     *
-     * @param $value
-     * @return string
-     * @date 2024/8/23 11:57
-     * @author 原点 467490186@qq.com
-     */
-    private function getPhpTypeName($value): string
-    {
-        $type = gettype($value);
-        return match ($type) {
-            'integer' => 'int',
-            'double' => 'float',
-            'boolean' => 'bool',
-            'NULL' => 'null',
-            default => $type,
-        };
-    }
-
-    /**
-     * 根据类型转换值并赋值给对象
-     *
-     * @param object $object
-     * @param string $key
-     * @param mixed $value
-     * @param array $types
-     * @throws ParameterException|ReflectionException
-     * @date 2024/8/28 11:23
-     * @author 原点 467490186@qq.com
-     */
-    private function assignConvertedValue(object $object, string $key, mixed $value, array $types): void
-    {
-        foreach ($types as $type) {
-            $typeName = $type->getName();
-            try {
-                if ($this->isConvertible($this->getPhpTypeName($value), $typeName) && settype($value, $typeName)) {
-                    $object->$key = $value;
-                    return;
-                } elseif (class_exists($typeName) && is_array($value)) {
-                    $object->$key = $this->arrayToObject($value, $typeName);
-                    return;
-                }
-            } catch (\Throwable $e) {
-                // 类型转失败，尝试下一个类型
-            }
-        }
-        throw new ParameterException("$key 类型不匹配");
-    }
-
-    /**
-     * 检查类型是否可以转换
-     *
-     * @param string $sourceType
-     * @param string $targetType
-     * @return bool
-     * @date 2024/8/30 09:32
-     * @author 原点 467490186@qq.com
-     */
-    private function isConvertible(string $sourceType, string $targetType): bool
-    {
-        if ($sourceType === $targetType) {
-            return true; // 同一类型可以转换
-        }
-        $conversionTable = [
-            'int'    => ['float', 'string', 'bool'],
-            'float'  => ['int', 'string', 'bool'],
-            'string' => ['int', 'float', 'bool'],
-            'bool'   => ['int', 'float', 'string'],
-            'array'  => ['object'],
-            'object' => ['array', 'string'],
-            'null'   => ['int', 'float', 'string', 'bool', 'array', 'object'],
-        ];
-
-        return in_array($targetType, $conversionTable[$sourceType] ?? [], true);
-    }
 
 }
